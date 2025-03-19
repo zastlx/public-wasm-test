7be0b98c20007e97f5d9fc6f12b62948b416c077
thonk <hello@villainsrule.xyz>
2025-03-19 17:05:53 -0400

    modify how test builds work

--- test.sh
+++ test.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-rm -rf ./builds/
-mkdir builds
+rm -f ./build/*.tgz
 
 version=$(jq -r '.version' package.json)
 buildId=$RANDOM
 
-npm pack --pack-destination ./builds/
+npm pack --pack-destination ./build/
 
-mv builds/yolkbot-${version}.tgz builds/yolkbot-${buildId}.tgz
+mv build/yolkbot-${version}.tgz build/yolkbot-${buildId}.tgz
 
 cd ../yolkdev || exit 1
 
 bun rm yolkbot
-bun i file:../yolkbot/builds/yolkbot-${buildId}.tgz
+bun i file:../yolkbot/build/yolkbot-${buildId}.tgz
 
 cd ../yolkbot || exit 1
