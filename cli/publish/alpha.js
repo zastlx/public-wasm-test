12a5fb78631fc01c7f9093282ed03f535b67f5f3
thonk <hello@villainsrule.xyz>
2025-04-08 22:05:48 -0400

    release 0.1.1 + new release webhook embed

--- cli/publish/alpha.js
+++ cli/publish/alpha.js
@@ -15,6 +15,7 @@ await fetch(webhook, {
         embeds: [{
             author: {
                 name: `${pkg.name} ${pkg.version.replace(/-([a-zA-Z]+)\./, ' / $1 ')}`,
+                url: `https://www.npmjs.com/package/yolkbot/v/${pkg.version}`,
                 icon_url: 'https://cdn.discordapp.com/icons/1342571038063460443/620ed351a0e43104c4ec8cc4a8304870.png?size=128'
             },
             color: 39902,
--- cli/publish/prod.js
+++ cli/publish/prod.js
@@ -7,17 +7,19 @@ import fs from 'node:fs';
 
 const pkg = JSON.parse(fs.readFileSync(new URL('../../package.json', import.meta.url)));
 
+const codeblock = '```';
+
 await fetch(webhook, {
     method: 'POST',
     body: JSON.stringify({
         embeds: [{
             author: {
-                name: 'npm package release',
-                icon_url: 'https://i.imgur.com/yl2AH1a.png'
+                name: `${pkg.name} ${pkg.version.replace(/-([a-zA-Z]+)\./, ' / $1 ')}`,
+                url: 'https://npmjs.com/yolkbot',
+                icon_url: 'https://cdn.discordapp.com/icons/1342571038063460443/620ed351a0e43104c4ec8cc4a8304870.png?size=128'
             },
-            title: `${pkg.name} v${pkg.version} has been published!`,
-            color: 16711680,
-            description: `**\`${pkg.version}\`** - https://npmjs.com/package/${pkg.name}\n\n\`npm install yolkbot@${pkg.version}\`\n\`bun add yolkbot@${pkg.version}\`\n\`yarn add yolkbot@${pkg.version}\``
+            color: 39902,
+            description: `${codeblock}npm install ${pkg.name}@${pkg.version}${codeblock}${codeblock}bun add ${pkg.name}@${pkg.version}${codeblock}${codeblock}yarn add ${pkg.name}@${pkg.version}${codeblock}\n**chec <#1343337711473528862> for the ${pkg.version} changelog!**`
         }]
     }),
     headers: {
