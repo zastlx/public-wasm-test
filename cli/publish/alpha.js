402c82ed872bcdf63aacc78619043205ae28f052
thonk <hello@villainsrule.xyz>
2025-04-30 23:02:58 -0400

    pnpm > yarn

--- cli/publish/alpha.js
+++ cli/publish/alpha.js
@@ -19,7 +19,7 @@ await fetch(webhook, {
                 icon_url: 'https://cdn.discordapp.com/icons/1342571038063460443/620ed351a0e43104c4ec8cc4a8304870.png?size=128'
             },
             color: 39902,
-            description: `${codeblock}npm install ${pkg.name}@${pkg.version}${codeblock}${codeblock}bun add ${pkg.name}@${pkg.version}${codeblock}${codeblock}yarn add ${pkg.name}@${pkg.version}${codeblock}\n**report cockroaches (& other bugs) in** <#1342571945123774527>`
+            description: `${codeblock}npm install ${pkg.name}@${pkg.version}${codeblock}${codeblock}bun add ${pkg.name}@${pkg.version}${codeblock}${codeblock}pnpm add ${pkg.name}@${pkg.version}${codeblock}\n**report cockroaches (& other bugs) in** <#1342571945123774527>`
         }]
     }),
     headers: {
