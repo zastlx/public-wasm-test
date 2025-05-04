9338baccdb69902aa2a9997124ad6880f3848cce
thonk <hello@villainsrule.xyz>
2025-05-03 20:14:44 -0400

    chunk up the `fetchMap` types

--- src/types/constants/maps.d.ts
+++ src/types/constants/maps.d.ts
@@ -1,88 +1,95 @@
+export interface MapModes {
+    FFA: boolean;
+    Teams: boolean;
+    Spatula: boolean;
+    King?: boolean;
+}
+
+export type MapAvailability = 'public' | 'private' | 'both';
+
 export interface Map {
     filename: string;
     hash: string;
     name: string;
-    modes: {
-        FFA: boolean;
-        Teams: boolean;
-        Spatula: boolean;
-        King?: boolean;
-    };
-    availability: 'public' | 'private' | 'both';
+    modes: MapModes;
+    availability: MapAvailability;
     numPlayers: string;
+}
+
+export interface MapData {
+    [type: string]: {
+        x: number;
+        y: number;
+        z: number;
+        rx?: number;
+        ry?: number;
+        rz?: number;
+    }
+}
+
+export interface MapSun {
+    direction: {
+        x: number;
+        y: number;
+        z: number;
+    }
+    color: string;
+}
+
+export interface MapRender {
+    AO?: string;
+    Diffuse: string;
+    Direct: string;
+    Dirt: string;
+    Environment?: string;
+    FogDensity: string;
+    Indirect: string;
+    Lights?: string;
+    LightsReflect?: string;
+    pointLightIntensity: number;
+    Reflect: string;
+    Sun?: string;
+    SunReflect?: string;
+}
+
+export interface MapExtents {
+    x: {
+        min: number;
+        max: number;
+    }
+    y: {
+        min: number;
+        max: number;
+    }
+    z: {
+        min: number;
+        max: number;
+    }
+    width: number;
+    height: number;
+    depth: number;
 }
 
 export interface MapJSON {
     fileVersion: number;
-    sun: {
-        direction: {
-            x: number;
-            y: number;
-            z: number;
-        }
-        color: string;
-    };
+    sun: MapSun;
     ambient: string;
     fog: {
         density: string;
         color: string;
-    };
-    data: {
-        [type: string]: {
-            x: number;
-            y: number;
-            z: number;
-            rx?: number;
-            ry?: number;
-            rz?: number;
-        }
-    };
-    palette: string[];
-    render: {
-        AO?: string;
-        Diffuse: string;
-        Direct: string;
-        Dirt: string;
-        Environment?: string;
-        FogDensity: string;
-        Indirect: string;
-        Lights?: string;
-        LightsReflect?: string;
-        pointLightIntensity: number;
-        Reflect: string;
-        Sun?: string;
-        SunReflect?: string;
     }
+    data: MapData;
+    palette: string[];
+    render: MapRender;
     width: number;
     height: number;
     depth: number;
     name: string;
     surfaceArea: number;
-    extents: {
-        x: {
-            min: number;
-            max: number;
-        }
-        y: {
-            min: number;
-            max: number;
-        }
-        z: {
-            min: number;
-            max: number;
-        }
-        width: number;
-        height: number;
-        depth: number;
-    }
+    extents: MapExtents;
     skybox: 'default' | 'moonbase' | 'night' | 'whimsical';
-    modes: {
-        FFA: boolean;
-        Teams: boolean;
-        Spatula: boolean;
-        King?: boolean;
-    }
-    availability: 'public' | 'private' | 'both';
+    modes: MapModes;
+    availability: MapAvailability;
     numPlayers: string;
 }
 
