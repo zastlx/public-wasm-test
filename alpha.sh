04e0c644c5d89262d8f3417bd3f27c6b5c092862
thonk <hello@villainsrule.xyz>
2025-05-13 23:32:00 -0400

    rewrite alpha tooling

--- alpha.sh
+++ alpha.sh
@@ -1,38 +1,15 @@
 latest_version=$(curl -s https://registry.npmjs.com/yolkbot | jq -r '.["dist-tags"].alpha')
 current_version=$(jq -r '.version' ./package.json)
 
-if [[ "$current_version" != *"-alpha."* ]]; then
-    echo "The version provided is not a valid alpha version."
-    exit 1
-fi
+new_version=$(echo $latest_version | awk -F- '{print $NF}')
+new_version=$((new_version + 1))
+new_version="0.0.0-$new_version"
 
-new_version=$(echo "$current_version" | awk -F. -v OFS=. '{$NF = $NF + 1; print}')
-sed -i '' "s/$current_version/$new_version/g" package.json
+echo "publishing $new_version"
 
-current_version=$new_version
-
-if [[ "$latest_version" == *"-alpha."* || "$current_version" == *"-alpha."* ]]; then
-    latest_version_numeric=$(echo "$latest_version" | sed 's/-alpha\.[0-9]*//')
-    current_version_numeric=$(echo "$current_version" | sed 's/-alpha\.[0-9]*//')
-
-    if [ "$latest_version_numeric" == "$current_version_numeric" ]; then
-        latest_alpha=$(echo "$latest_version" | sed 's/.*-alpha\.//')
-        current_alpha=$(echo "$current_version" | sed 's/.*-alpha\.//')
-
-        if [ "$current_alpha" -le "$latest_alpha" ]; then
-            echo "The current alpha version must be greater than the latest published alpha version."
-            exit 1
-        fi
-    fi
-fi
-
-if [ "$(printf '%s\n' "$latest_version" "$current_version" | sort -V | head -n1)" != "$latest_version" ]; then
-    echo "The current version must be greater than the latest published version."
-    exit 1
-fi
+sed -i '' "s/$latest_version/$new_version/g" package.json
 
 bun cli/build.js
 bun browser/compile.js
 
 npm publish --tag alpha
-bun --env-file=.env cli/publish/alpha.js
